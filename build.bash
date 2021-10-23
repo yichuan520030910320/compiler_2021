0d8834e962dd8f9cbc50e2226d1103544180224c
yichuan~ <73766326+yichuan520030910320@users.noreply.github.com>
2021-10-23 23:03:19 +0800

    bash

--- build.bash
+++ build.bash
@@ -1,4 +1,7 @@
+# this script is called when the judge is building your compiler.
+# no argument will be passed in.
+# shellcheck disable=SC2121
 set ff=UNIX
 set -e
-mkdir -p bin
-find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.9.1-complete.jar @/dev/stdin
+mkdir runContain
+find ./src -name "*.java" -print0|xargs -0 javac -d runContain
--- semantic.bash
+++ semantic.bash
@@ -1,3 +1,4 @@
+# shellcheck disable=SC2121
 set ff=UNIX
 set -e
-cat | java -cp /ulib/java/antlr-4.9.1-complete.jar:./bin Main -semantic
+java -cp ./runContain Main
