f9bf6a6444b3f3ed6a41e1751dc1b0fd31efbfbb
yichuan~ <73766326+yichuan520030910320@users.noreply.github.com>
2021-12-08 00:50:53 +0800

    draft br

--- src/IR/IRbasicblock/IRbasicblock.java
+++ src/IR/IRbasicblock/IRbasicblock.java
@@ -2,15 +2,28 @@ package IR.IRbasicblock;
 
 import IR.IRfunction.IRfunction;
 import IR.Instru.BaseInstru;
+import IR.Operand.BaseUser;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 
-public class IRbasicblock {
+public class IRbasicblock extends BaseUser {//可以记录其中的instru 使用
     public String blockname;
+    //point to the function
     public IRfunction iRfunction;
+    //the instruction index
     public LinkedList<BaseInstru> link_in_basicblock;
+    //terminal instru temporarily don't know how to use it
     public BaseInstru terminal_instruction;
+    //jump from where
+    public HashSet<IRbasicblock> pre_basicblock;
+    //jump to where
+    public HashSet<IRbasicblock> nxt_basic_block;
 
 
 
+    @Override
+    public String toString() {
+        return blockname;
+    }
 }
--- src/IR/Instru/BrInstruction.java
+++ src/IR/Instru/BrInstruction.java
@@ -2,10 +2,27 @@ package IR.Instru;
 
 import IR.IRbasicblock.IRbasicblock;
 import IR.IRvisitor;
+import IR.Operand.BaseOperand;
+import IR.Operand.Register;
 
-public class BrInstruction extends BaseInstru{
-    public BrInstruction(IRbasicblock iRbasicblock) {
+public class BrInstruction extends BaseInstru {
+    public BaseOperand cond;
+    public IRbasicblock true_block;
+    public IRbasicblock false_block;
+    public BrInstruction(IRbasicblock iRbasicblock,BaseOperand cond_,IRbasicblock true_block_,IRbasicblock false_block_) {
         super(iRbasicblock);
+        cond=cond_;
+        true_block=true_block_;
+        false_block=false_block_;
+        if (cond!=null){
+            if (cond instanceof Register) cond.Add_be_used(this);
+            Current_Basicblock.nxt_basic_block.add(false_block);
+            false_block.pre_basicblock.add(Current_Basicblock);
+            false_block.Add_def(this);
+        }
+        true_block.Add_def(this);
+        Current_Basicblock.nxt_basic_block.add(true_block);
+        true_block.pre_basicblock.add(Current_Basicblock);
     }
 
     @Override
@@ -13,9 +30,11 @@ public class BrInstruction extends BaseInstru{
         visitor.visit(this);
     }
 
+//    br label %returnBlock.0  || br i1 %greaterThan.0, label %ifThenBlock.0, label %ifElseBlock.0
     @Override
     public String toString() {
-        return null;
+        if (cond==null)return "br label %"+true_block.toString();
+        else return "br "+cond.unit_output()+", label %"+true_block.toString()+", label %"+false_block.toString();
     }
 
 
