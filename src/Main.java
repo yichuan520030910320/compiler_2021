6cd8763f33c967a57ca06b4d44ed784b5e04fcc2
yichuan~ <73766326+yichuan520030910320@users.noreply.github.com>
2022-01-19 00:31:42 +0800

    oj

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         //chose the read option
-        boolean localjudge =true;
+        boolean localjudge =false;
         String name;
         InputStream input = null;boolean onlysemnatic=false;
         if (localjudge ) {
